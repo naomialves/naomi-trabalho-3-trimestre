462a03c5561bd33103eeb0c3030730e8bf152124
naoalves <145606088+naomialves@users.noreply.github.com>
2024-09-17 16:20:40 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'QUEM FOI KAFKA?',
+    'Franz Kafka foi um escritor boêmio de língua alemã, autor de romances e contos, considerado pelos críticos como um dos escritores mais influentes do século XX.'
+)
+
+criaCartao(
+    'Frances',
+    'Qual a tradução da frase "LA VIE EST UN SOUFFLE"?',
+    'A VIDA É UM SOPRO'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é uma função?',
+    'Uma função é uma relação de um conjunto com um conjunto. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bye em ingles?',
+    'Oi em ingles é bye'
+)
